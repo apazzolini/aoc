42ced97ad7e004261ad4d305a5097121fdb09398
Andre Azzolini <andre@azzolini.io>
2021-12-18 11:57:16 -0600

    Solve 2021 day 7

---
+++ src/2021/day07/day07.js
@@ -0,0 +1,54 @@
+import _ from 'lodash' // eslint-disable-line
+import * as U from '../../utils.js' // eslint-disable-line
+
+function parseInput(input) {
+  return input.split(',').map(Number)
+}
+
+function calcFuelLinear(crabs, pos) {
+  let fuel = 0
+
+  for (const c of crabs) {
+    fuel += Math.abs(c - pos)
+  }
+
+  return fuel
+}
+
+function calcFuelExponential(crabs, pos) {
+  let fuel = 0
+
+  for (const c of crabs) {
+    const diff = Math.abs(c - pos)
+    fuel += (diff * (diff + 1)) / 2
+  }
+
+  return fuel
+}
+
+function findPos(crabs, fuelCalculator) {
+  let min = _.min(crabs)
+  let max = _.max(crabs)
+
+  let ans = { fuel: Number.MAX_SAFE_INTEGER, pos: 0 }
+
+  for (let i = min; i <= max; i++) {
+    const fuel = fuelCalculator(crabs, i)
+    if (fuel < ans.fuel) {
+      ans.fuel = fuel
+      ans.pos = i
+    }
+  }
+
+  return ans.fuel
+}
+
+export function solvePart1(input) {
+  let crabs = parseInput(input)
+  return findPos(crabs, calcFuelLinear)
+}
+
+export function solvePart2(input) {
+  let crabs = parseInput(input)
+  return findPos(crabs, calcFuelExponential)
+}
---
+++ src/2021/day07/day07.test.js
@@ -0,0 +1,18 @@
+import { solvePart1, solvePart2 } from './day07.js' // eslint-disable-line
+import * as U from '../../utils.js' // eslint-disable-line
+
+describe('2021 day 07', () => {
+  const example = `16,1,2,0,4,2,7,1,2,14`
+
+  describe('part 1', () => {
+    test('solves an example', () => {
+      expect(solvePart1(example)).toEqual(37)
+    })
+  })
+
+  describe('part 2', () => {
+    test('solves an example', () => {
+      expect(solvePart2(example)).toEqual(168)
+    })
+  })
+})
